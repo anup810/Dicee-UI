161cc58872c78ef0da1b5829999a1f4524716dbd
Anup Saud <anupsaud10@gmail.com>
2024-07-01 11:50:24 -0400

    Initial Commit

---
+++ Dicee UI/Dicee_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_UIApp.swift
+//  Dicee UI
+//
+//  Created by Anup Saud on 2024-07-01.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
